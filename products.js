9c2a5690dad91b9ef92fa42b6c6ae98f3b00b611
MAURICIO GALVEZ <maugalvez89@gmail.com>
2023-02-09 19:56:37 -0500

    terminado

---
+++ products.js
@@ -0,0 +1,33 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Tomate",
+      precio: 50,
+      img:
+        "https://tse1.mm.bing.net/th?id=OIP.x8W7ld_ncCGWCJiSjyoJ8gHaGM&pid=Api&P=0",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Atún",
+      precio: 100,
+      img:
+        "https://tse1.mm.bing.net/th?id=OIP.l4V3HbJGdfjO1FmydZIZ4AHaEf&pid=Api&P=0",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Cerveza",
+      precio: 150,
+      img:
+        "https://tse2.mm.bing.net/th?id=OIP.zxRHqF6vafZxwSlIejOp3AHaE8&pid=Api&P=0"
+    },
+    {
+      id: 4,
+      nombre: "Leche",
+      precio: 200,
+      img:
+        "https://tse4.mm.bing.net/th?id=OIP.m4vL8Q8hSNC6nZ9g_u0F5AHaEi&pid=Api&P=0",
+      cantidad: 1,
+    },
+  ];
